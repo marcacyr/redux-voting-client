e2daae8b53a5338081c8ff10b2952093aab0b567
Marc Cyr <marcacyr@gmail.com>
2016-01-01 23:59:55 -0800

    add react router and route for current functionality. add results page and corresponding route. add tests.

---
+++ src/components/Results.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {List} from 'immutable';
+
+export default React.createClass({
+    mixins: [PureRenderMixin],
+
+    getPair() {
+        return this.props.pair || List();
+    },
+
+    getVotes(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    },
+
+    render() {
+        return (
+            <div className='results'>
+                { this.getPair().map(entry =>
+                    <div key={ entry } className='entry'>
+                        <h1>{ entry }</h1>
+                        <div className='voteCount'>
+                            { this.getVotes(entry) }
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 import {List} from 'immutable';
+import Results from './components/Results';
 
-const pair = List.of('Trainspotting', '28 Days Later');
+const routes =
+    <Route component={App}>
+        <Route path='/results' component={ Results } />
+        <Route path='/' component={ Voting } />
+    </Route>;
 
 ReactDOM.render(
-    <Voting pair={ pair } winner='Trainspotting' />,
+    <Router>{ routes }</Router>,
     document.getElementById('app')
 );
